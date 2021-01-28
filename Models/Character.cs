9556c5f7f094d64e111baafa3e57990cc9c23796
Kamil Siemion <kamil.siemion@gmail.com>
2021-01-28 10:06:15 +0100

    Character model, controller, services

---
+++ Models/Character.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarWars.Models
+{
+    public class Character
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string[] Episodes { get; set; }
+        public string[] Friends { get; set; }
+
+    }
+}
